5579b33c7d1f5e7ae79795a0a6b2f69f08229546
Geoff Cox <slangingcode@gmail.com>
2016-12-19 22:09:05 -0500

    Create home view and template

--- blather/settings.py
+++ blather/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'blather.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['blat/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
