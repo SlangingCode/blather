520562d471714e1551a35b68ed6f53996374dbf8
Geoff Cox <slangingcode@gmail.com>
2016-12-20 14:31:17 -0500

    Deployment of inital Rock Me Out webapp

--- blather/wsgi.py
+++ blather/wsgi.py
@@ -12,7 +12,8 @@ import os, sys
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blather.settings")
-#sys.path.append('/var/www/blather/blaher')
-#sys.path.append('/var/www/blather')
+sys.path.append('/var/www/geoffcox77.com/blather/blat/')
+sys.path.append('/var/www/geoffcox77.com/blather/rockmeout/')
+sys.path.append('/var/www/geoffcox77.com/blather/')
 
 application = get_wsgi_application()
